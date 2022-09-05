2369b69b576b41cc8520011b21a50ce15263fee1
nils865 <nils.staas0806@gmail.com>
2022-09-05 14:22:09 +0200

    updated build script

--- build.bat
+++ build.bat
@@ -1,10 +1,20 @@
+@echo off
+echo compiling scripts...
 cd .\src\
 gcc -Wall -c lib.c
 gcc -Wall -c main.c
+echo compiled scripts
+echo:
 
+echo building exe...
 cd ..\build\
 gcc -o stoopid ..\src\lib.o ..\src\main.o
+echo build finished
+echo:
 
 cd ..\src\
 del lib.o
 del main.o
+echo successfully build exe
+echo:
+@echo on
