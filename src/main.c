26826024f3a0b77e5fd5e0ace2a04134ea08f905
nils865 <nils.staas0806@gmail.com>
2022-09-06 11:59:20 +0200

    removed unused includes

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <stdarg.h>
-#include <time.h>
-#include <unistd.h>
 #include "lib.h"
 #include "global.h"
 
